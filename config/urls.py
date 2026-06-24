6e97b4f5e2cad80c68bfdf7f06e701a7c88cffe5
andreafilipponi04 <fibre_religiose8t@icloud.com>
2026-06-24 16:23:06 +0200

    Add redirect

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -7,10 +8,15 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('polls.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('api/profiles/', include('profiles.urls')),
+    path("admin/", admin.site.urls),
+    path("api/", include("polls.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("api/profiles/", include("profiles.urls")),
+    path(
+        "",
+        RedirectView.as_view(url="api/polls/", permanent=False),
+        name="root-redirect",
+    ),
 ]
